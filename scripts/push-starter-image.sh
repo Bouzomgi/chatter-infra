b73c2a831fe55ac017428398e6ab74f20ae48f69
Brian Ouz <bouzomgi@gmail.com>
2024-12-06 19:11:25 -0500

    use MY imagegit add .

--- scripts/push-starter-image.sh
+++ scripts/push-starter-image.sh
@@ -4,10 +4,10 @@
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 # Pull the starter Docker image
-docker image pull yeasy/simple-web
+docker image pull bouzomgi/micro-server
 
 # Tag the image for ECR
-docker tag yeasy/simple-web $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BACKEND_ECR_REPO_NAME
+docker tag bouzomgi/micro-server $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BACKEND_ECR_REPO_NAME
 
 # Push the image to ECR
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BACKEND_ECR_REPO_NAME
